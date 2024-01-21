3a6d06ed9512b6c48ae405701ba3ec2b4e0a6f18
AL <32211153+iluvjava@users.noreply.github.com>
2024-01-21 01:29:24 -0800

    >w< Home Desktop

---
+++ src_python/scratchpaper.py
@@ -0,0 +1,25 @@
+import numpy
+import sys
+import scipy
+import scipy.sparse
+
+
+def main(): 
+    pass
+
+"""
+Psuedo code for making a numpy sparse matrix. 
+
+"""
+def make_matrix():
+    CsrSp = scipy.sparse.csr_matrix
+    
+    return None
+
+
+
+if __name__ == "__main__":
+    print(f"Dir at: {__file__}")
+    main()
+
+
