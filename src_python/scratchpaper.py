83f735999417b1be95994f43692d5a691607e719
AL <32211153+iluvjava@users.noreply.github.com>
2024-01-23 22:19:33 -0800

    >w<

--- src_python/scratchpaper.py
+++ src_python/scratchpaper.py
@@ -1,25 +1,55 @@
-import numpy
+import numpy as np
 import sys
 import scipy
 import scipy.sparse
+import itertools
 
 
 def main(): 
     pass
 
+
+
+
+
 """
 Psuedo code for making a numpy sparse matrix. 
+This is for making the constraints matrix for the non-smooth part with the one-norm. 
+
+parameters
+---
+CoeffMtx: A matrix that can be indexed by as [i, j], it should be the estimated transition probability matrix via maximum likelihood without the penalization term. 
 
 """
-def make_matrix():
-    CsrSp = scipy.sparse.csr_matrix
-    
-    return None
+def make_matrix(CoeffMtx, regl=1.0):
+    if not(type(CoeffMtx) == np.ndarray and CoeffMtx.ndim == 2):
+        raise ValueError("Gives MtxCoeff as a 2 Dim numpy.ndarray")
+    if np.size(CoeffMtx, 0) != np.size(CoeffMtx, 1): 
+        raise ValueError("Expect MtxCoeff as a squared numpy.ndarray matrix ")
+    regl = float(regl) # type stablization. 
+    if regl < 0: 
+        raise ValueError(f"Expect regularization term to be > 0 but get regularization={regl}")
+    csr_matrix = scipy.sparse.csr_matrix
+    n = np.size(CoeffMtx, 0)
+    row, col, data = [], [], []
+    C = CoeffMtx.reshape((-1,)) # flatten the coefficient matrix into a vector. 
+    for k, (i, j) in enumerate(itertools.combinations(range(n**2), 2)): 
+        if C[i] - C[j] == 0: 
+            # still tells the sparse matrix parser that there is a zero row there. 
+            row.append(k); col.append(0); data.append(0)
+        row.append(k); col.append(i); data.append(regl/abs(C[i] - C[j]))
+        row.append(k); col.append(j); data.append(-regl/abs(C[i] - C[j]))
+        pass
+    return csr_matrix((data, (row, col)), dtype=float)
 
+
+"""
+Given a list of eta_i for the maximal likelihood, and the MLE estimated transition probability matrix P that is n by n, the function formulate the problems for scipy.optimize. 
+"""
+def formulate_SQP():
+    pass
 
 
 if __name__ == "__main__":
-    print(f"Dir at: {__file__}")
+    print(f"Running file at: {__file__}")
     main()
-
-
