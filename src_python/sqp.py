ce608e0f575a124ac6580bfd68c72dbb3be2a7f9
alto <victor13011660040@gmail.com>
2024-02-05 17:09:11 -0800

    >w< wdskpt

--- src_python/sqp.py
+++ src_python/sqp.py
@@ -116,14 +116,15 @@ def make_matrix(CoeffMtx, regl=1.0):
     row, col, data = [], [], []
     C = CoeffMtx.reshape((-1,)) # flatten the coefficient matrix into a vector. 
     for k, (i, j) in enumerate(itertools.combinations(range(n**2), 2)): 
-        if C[i] - C[j] == 0: 
-            # still tells the sparse matrix parser that there is a zero row there. 
-            row.append(k); col.append(i); data.append(0)
-            row.append(k); col.append(j); data.append(0)
-        else:
-            row.append(k); col.append(i); data.append(regl/abs(C[i] - C[j]))
-            row.append(k); col.append(j); data.append(-regl/abs(C[i] - C[j]))
-        pass
+        # if C[i] - C[j] == 0: 
+        #     # still tells the sparse matrix parser that there is a zero row there. 
+        #     row.append(k); col.append(i); data.append(0)
+        #     row.append(k); col.append(j); data.append(0)
+        # else:
+        #     row.append(k); col.append(i); data.append(regl/abs(C[i] - C[j]))
+        #     row.append(k); col.append(j); data.append(-regl/abs(C[i] - C[j]))
+        row.append(k); col.append(i); data.append(regl/abs(C[i] - C[j] + 1e-8))
+        row.append(k); col.append(j); data.append(-regl/abs(C[i] - C[j] + 1e-8))
     return csr_matrix((data, (row, col)), dtype=float)
 
 
